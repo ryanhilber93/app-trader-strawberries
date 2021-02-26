c8c1b69e9d4223cf33a8120927de363491da7d5d
fantahunteka1980 <fantutero@gmail.com>
2021-02-25 21:19:49 -0600

    final

--- data/app_trader.sql
+++ data/app_trader.sql
@@ -3,6 +3,9 @@ from app_store_apps;
 select *
 from play_store_apps;
 
+
+
+
 select ap.name, ap.price,pl.name,pl.price
 from app_store_apps as ap
 inner join play_store_apps as pl 
@@ -20,25 +23,69 @@ select a.name , a.primary_genre as app_primary_genre,  a.rating as app_rating,
 	  SELECT CAST(TRIM(REPLACE(price, '$', '')) AS numeric) AS cleaned_price
 FROM play_store_apps
  
-
+****Ryan****
 SELECT name, price, rating,
 CASE WHEN CAST(TRIM(REPLACE(price, '$', '')) AS numeric) < 1 THEN 1500*(12*(1+2*CAST(rating AS numeric))) - 10000
 WHEN CAST(TRIM(REPLACE(price, '$', '')) AS numeric) >= 1 THEN 1500*(12*(1+2*CAST(rating AS numeric))) - 10000 * CAST(TRIM(REPLACE(price, '$', '')) AS numeric) END as expected_revenue
 FROM play_store_apps
 WHERE rating IS NOT null
 ORDER BY expected_revenue DESC;
-WITH psa AS (SELECT DISTINCT name AS psa_name, category, price AS psa_price,rating AS psa_rating,
-			 review_count AS psa_review_count
-		  	 FROM play_store_apps
-	 		 WHERE rating > 4
-				AND price::money < 1:: money
-				AND review_count > 100000
-				AND category ILIKE 'Game'),
-	 asa AS (SELECT DISTINCT name AS asa_name, primary_genre AS category , price AS asa_price,
-			 rating AS asa_rating, review_count AS asa_review_count
-	  		 FROM app_store_apps
-  	  		 WHERE rating > 4
-				AND price::money < 1:: money
-				AND review_count::numeric > 100000
-	 			AND primary_genre ILIKE 'Games');
+
+
+*******************Joshua*****************************************
+SELECT name,
+		'Google Play' AS store,
+		CAST(TRIM(REPLACE(price, '$', '')) AS numeric(5,2)) AS cleaned_price,
+		review_count,
+		rating,
+		content_rating,
+		genres
+FROM play_store_apps
+UNION ALL
+SELECT name,
+		'Apple Store' AS store,
+		price,
+		CAST(review_count AS int),
+		rating,
+		content_rating,
+		primary_genre
+FROM app_store_apps;
+**************Ryan***************************
+SELECT name, price, rating, primary_genre, CAST(review_count AS numeric) AS cleaned_review_count,
+CASE WHEN price < 1 THEN 1500*(12*(1+2*rating)) - 10000 
+ELSE 1500*(12*(1+2*rating)) - 10000 * price END as expected_profit
+FROM app_store_apps
+WHERE name IN 
+	(SELECT name
+	FROM app_store_apps
+	INTERSECT
+	SELECT name
+	FROM play_store_apps)
+ORDER BY expected_profit DESC, cleaned_review_count DESC;
+*************Joshua************************
+Rounding to half-values:
+SELECT rating,
+		ROUND(ROUND(2*rating)/2,1) AS clean_rating
+FROM play_store_apps;
+****************************************************8888*******
+
+SELECT a.name as name , a.primary_genre as app_primary_genre,  a.rating as app_rating, 
+	   a.content_rating as app_content_rating,a.price as app_price,
+       a.currency as app_currency,a.review_count as app_review_count
+	   from app_store_apps;
+	   
+	   
+	   b.review_count as play_review_count,
+	   b.genres as play_genre, b.rating as play_rating,b.price as play_price,
+	   b.content_rating as play_content_rating,b.type as play_type,b.install_count as play_install_count;
  
+ /* trying to see if there is some thing in install count and review count This is my part of the assignment*/
+ 
+select distinct a.name, a.review_count::int, /*b.install_count,*/ max(b.review_count) as max_review_count , a.rating        
+FROM app_store_apps a
+inner join play_store_apps b
+on a.name = b.name
+group by a.name,a.review_count::int,a.rating
+ORDER BY /*b.install_count desc,*/a.review_count::int desc, max_review_count desc,a.rating desc
+ limit 10;
+*********
